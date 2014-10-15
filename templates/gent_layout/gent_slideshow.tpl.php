c7e5eababe63be322ab2a510e06e6e82e126eefd
fwalraet <floris.walraet@district01.be>
2014-10-15 14:32:25 +0200

    Fixed position of caption

--- templates/gent_layout/gent_slideshow.tpl.php
+++ templates/gent_layout/gent_slideshow.tpl.php
@@ -4,23 +4,23 @@
   <?php if ($item_count > 0): ?>
     <?php $item = array_shift($items); ?>
     <div class="l-full">
-      <figure class="mask ratio--big-thumb">
+      <div class="mask ratio--big-thumb">
         <a rel="gallery-1" href="<?php print $item['image_full'];?>" class="swipebox" title="<?php echo $item['title'];?>">
          <img src="<?php print $item['image_medium'];?>" alt="<?php echo $item['alt'];?>"/>
         </a>
-        <figcaption class="caption"><?php echo $item['title'];?></figcaption>
-      </figure>
+      </div>
+      <p class="caption"><?php echo $item['title'];?></p>
     </div>
   <?php endif; ?>
 
   <?php if ($item_count > 1): ?>
     <div class="l-thumb">
     <?php foreach ($items as $num => $item): ?>
-      <figure class="mask ratio--thumb">
+      <div class="mask ratio--thumb">
         <a href="<?php print $item['image_full'];?>" rel="gallery-1" class="swipebox" title="<?php echo $item['title'];?>">
           <img src="<?php print $item['image_thumbnail'];?>" alt="<?php echo $item['alt'];?>"  />
         </a>
-      </figure>
+      </div>
     <?php endforeach; ?>
     </div>
   <?php endif; ?>
