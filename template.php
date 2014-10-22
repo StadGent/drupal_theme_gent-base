d29e0c4350a79696dec9219de01f4b902ae7c9f2
fwalraet <floris.walraet@district01.be>
2014-10-22 13:08:50 +0200

    Changed alert styling

--- template.php
+++ template.php
@@ -341,3 +341,40 @@ function gent_base_form_gent_newsletter_form_alter(&$form) {
   $form['email']['#attributes']['class'][] = 'prefix--email';
   $form['submit']['#attributes']['class'] = array('btn', 'btn--medium', 'btn--alpha', 'postfix--email-submit');
 }
+
+/**
+ * Implements theme_status_messages().
+ *
+ * @ingroup themeable
+ *
+ * Override the classes to be more SMACCS-like.
+ */
+function gent_base_status_messages($variables) {
+  $display = $variables['display'];
+  $output = '';
+
+  $status_heading = array(
+    'status' => t('Status message'),
+    'error' => t('Error message'),
+    'warning' => t('Warning message'),
+  );
+  foreach (drupal_get_messages($display) as $type => $messages) {
+    $output .= "<div class=\"messages messages--$type\">\n";
+    if (!empty($status_heading[$type])) {
+      $output .= '<h2 class="element-invisible">' . $status_heading[$type] . "</h2>\n";
+    }
+    if (count($messages) > 1) {
+      $output .= " <ul>\n";
+      foreach ($messages as $message) {
+        $output .= '  <li>' . $message . "</li>\n";
+      }
+      $output .= " </ul>\n";
+    }
+    else {
+      $output .= $messages[0];
+    }
+    $output .= "</div>\n";
+  }
+  return $output;
+}
+
