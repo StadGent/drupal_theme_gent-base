305cf424109b578e294d59167a11cb7c807e9656
fwalraet <floris.walraet@district01.be>
2014-10-23 15:45:40 +0200

    Chenged newsletter block

--- templates/gent_newsletter/gent_newsletter_block.tpl.php
+++ templates/gent_newsletter/gent_newsletter_block.tpl.php
@@ -4,13 +4,13 @@
  */
 ?>
 <div class="l-fourth-alt island island--theta js-height">
+
+  <?php if (isset($image)): ?>
 	<div class="island__top mask js-equal">
-	  <?php if (isset($sfeerbeeld)): ?>
-			 <?php print render($sfeerbeeld);?>
-	  <?php else: ?>
-	     <img src="<?php print base_path() . path_to_theme() ?>/img/jpg/flowers-small.jpg">
-	  <?php endif; ?>
+		<?php print $image;?>
 	</div>
+	<?php endif; ?>
+
 	<?php if ($caption): ?>
 	<div class="island__bottom js-equal">
 		<h2 class="hN"><?php print $caption ?></h2>
