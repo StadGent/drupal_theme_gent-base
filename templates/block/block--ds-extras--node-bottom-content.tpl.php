493db670d4b701cb96872f4462dcf4b8742db5ae
fwalraet <floris.walraet@district01.be>
2014-10-24 15:09:52 +0200

    Misch fixes

--- templates/block/block--ds-extras--node-bottom-content.tpl.php
+++ templates/block/block--ds-extras--node-bottom-content.tpl.php
@@ -2,6 +2,8 @@
 /**
  * This is a template for the Display Suite block region "Node bottom content". If used in a Ds view mode, it will render i
  * in the bottom content region without a title.
- */?>
+ */
+?>
   <?php print $content ?>
+
 
