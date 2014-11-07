92de2068f84dcbc130670c5f93b12d1fe3539ace
fwalraet <floris.walraet@district01.be>
2014-11-07 17:34:40 +0100

    GENTBE-686 - Afwerking gentinfo block

--- templates/gent_gentinfo/gentinfo_normal_block.tpl.php
+++ templates/gent_gentinfo/gentinfo_normal_block.tpl.php
@@ -6,31 +6,27 @@
 ?>
 
 <?php if ($settings['intro']): ?>
-  <p><?php print filter_xss($settings['intro'], array('p', 'strong', 'a', 'li', 'ul'))?></p>
+  <p class="column"><?php print filter_xss($settings['intro'], array('p', 'strong', 'a', 'li', 'ul'))?></p>
 <?php endif; ?>
 
-<ul class="no-bullet-list">
+<ul class="no-bullet-list column address">
 <?php if ($settings['phone']): ?>
 	<li>
 		<a href="tel:<?php print $settings['phone_full']; ?>" itemprop="telephone"><i class="icon-phone"></i><?php print check_plain($settings['phone']) ?></a>
 	</li>
 <?php endif; ?>
-
 <?php if ($settings['email']): ?>
 	<li>
 		<a href="<?php print url('mailto:' . $settings['email'], array('absolute' => TRUE));?>"><i class="icon-email"></i><?php print $settings['email'] ?></a>
 	</li>
 <?php endif; ?>
+</ul>
 
+<ul class="no-bullet-list contact">
 <?php if ($settings['contact_enabled']): ?>
 	<li>
-		<a href="<?php print url($settings['contact_link']) ?>"><i class="icon-link"></i><?php print check_plain($settings['contact_label']) ?></a>
+		 <a href="<?php print url($settings['contact_link']) ?>" class="btn btn--medium btn--delta"><span class="btn-centered"><i class="icon-link"></i><?php print check_plain($settings['contact_label']) ?></span></a>
 	</li>
 <?php endif; ?>
 
-<?php if ($settings['button_enabled']): ?>
-	<li>
-		<a href="<?php print url($settings['button_link']) ?>" class="btn btn--medium btn--delta olark-toggle"><i class="icon-bubble"></i><?php print check_plain($settings['button_label']) ?></a>
-	</li>
-<?php endif; ?>
 </ul>
