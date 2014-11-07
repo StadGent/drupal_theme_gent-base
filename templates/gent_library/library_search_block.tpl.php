b40f852ba38849d430545d5aa0e162e1f49ea135
fwalraet <floris.walraet@district01.be>
2014-11-07 15:13:16 +0100

    GENTBE-696 - Rethemed library block

---
+++ templates/gent_library/library_search_block.tpl.php
@@ -0,0 +1,9 @@
+<div class="library-search-box island island--zeta">
+  <form accept-charset="UTF-8" id="library-search" method="get" action="<?php print $url ?>" target="_blank">
+    <div class="island__body clearfix">
+      <label for="location-name"><?php print t('Search in the catalog') ?></label>
+      <input type="text" class="prefix--medium" maxlength="60" size="60" value="" name="<?php print $query_argument ?>" id="location-name" placeholder="<?php print t('Enter a searchterm') ?>">
+      <input type="submit" class="btn btn--medium btn--alpha postfix--medium" value="<?php print t('Search') ?>" id="edit-submit">
+    </div>
+  </form>
+</div>
