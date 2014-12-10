06951a6d4e563191f577cf9b74ec16c388d74e1c
Pieter Jan <pieter.jan.claeys@crosscheck.be>
2014-12-10 11:53:21 +0100

    GENTBE-766: Notice fix for link.

--- templates/gent_social/gent_social_media_summary_block.tpl.php
+++ templates/gent_social/gent_social_media_summary_block.tpl.php
@@ -9,7 +9,7 @@
 <?php foreach ($links as $link): ?>
 	<li>
 		<a href="<?php print url($link['url'], array('absolute' => TRUE))?>" class="gent-social-large"><i class="icon-<?php print drupal_html_class($link['type']) ?>"></i><?php print check_plain($link['display']); ?></a>
-        <a href="<?php print url($link['url'], array('absolute' => TRUE))?>" class="gent-social-small"><i class="icon-<?php print drupal_html_class($link['type']) ?>"></i><?php print ($link['display_small'])?check_plain($link['display_small']):check_plain($link['display']); ?></a>
+        <a href="<?php print url($link['url'], array('absolute' => TRUE))?>" class="gent-social-small"><i class="icon-<?php print drupal_html_class($link['type']) ?>"></i><?php print isset($link['display_small'])?check_plain($link['display_small']):check_plain($link['display']); ?></a>
 	</li>
 <?php endforeach; ?>
 </ul>
