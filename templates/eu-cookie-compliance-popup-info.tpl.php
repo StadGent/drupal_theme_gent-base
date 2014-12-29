4c65f1d6382a08c1d224820beb7df3ac8b572421
Pieter Jan <pieter.jan.claeys@crosscheck.be>
2014-12-29 14:01:21 +0100

    GENTBE-828: Added cookie compliance support in base theme.

---
+++ templates/eu-cookie-compliance-popup-info.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @file
+ * This is a template file for a pop-up prompting user to give their consent for
+ * the website to set cookies.
+ *
+ * When overriding this template it is important to note that jQuery will use
+ * the following classes to assign actions to buttons:
+ *
+ * agree-button      - agree to setting cookies
+ * find-more-button  - link to an information page
+ *
+ * Variables available:
+ * - $message:  Contains the text that will be display whithin the pop-up
+ * - $agree_button: Contains agree button title
+ * - $disagree_button: Contains disagree button title
+ */
+?>
+<div>
+  <div class ="popup-content info">
+    <div id="popup-text">
+      <?php print $message ?>
+    </div>
+    <div id="popup-buttons">
+      <input type="submit" class="form-submit agree-button" value="<?php print $agree_button; ?>">
+      <input type="submit" class="form-submit find-more-button" value="<?php print $disagree_button; ?>">
+    </div>
+  </div>
+</div>
