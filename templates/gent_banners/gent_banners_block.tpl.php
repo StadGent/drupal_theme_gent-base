6d1208905ffcbcbafdbdfa372f7f326af87d0a9a
fwalraet <floris.walraet@district01.be>
2015-03-12 13:23:31 +0100

    GENTBE-982 - Added library block styling, removed banners template

--- templates/gent_banners/gent_banners_block.tpl.php
+++
@@ -1,8 +0,0 @@
-<?php foreach ($items as $item): ?>
-  <div class="l-third island island--alpha js-height">
-    <h2 class="h1 hT"><?php print $item['title'] ?></h2>
-    <?php print render($item['content']); ?>
-  </div>
-<?php endforeach; ?>
-
-
--- templates/gent_library/library_search_block.tpl.php
+++ templates/gent_library/library_search_block.tpl.php
@@ -2,6 +2,7 @@
   <form accept-charset="UTF-8" id="library-search" method="get" action="<?php print $url ?>">
     <div class="island__body clearfix">
       <label for="location-name"><?php print t('Search in the catalog') ?></label>
+      <p><?php print t('Looking for books, films, music, the library of Ghent has something for everyone.') ?></p>
       <input type="text" class="prefix--medium" maxlength="60" size="60" value="" name="<?php print $query_argument ?>" id="location-name" placeholder="<?php print t('Enter a searchterm') ?>">
       <input type="submit" class="btn btn--medium btn--alpha postfix--medium" value="<?php print t('Search') ?>" id="edit-submit">
     </div>
