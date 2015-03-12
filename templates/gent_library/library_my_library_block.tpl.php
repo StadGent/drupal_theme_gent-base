b9069610b147db906b8fef29947314005bb5bc14
fwalraet <floris.walraet@district01.be>
2015-03-12 15:04:17 +0100

    GENTBE-982 - Added My library block template & styling

---
+++ templates/gent_library/library_my_library_block.tpl.php
@@ -0,0 +1,9 @@
+<div class="gent-library-block library-my-library-block island island--zeta">
+  <div class="island__body clearfix">
+    <h2><?php print t('My library') ?></h2>
+    <p><?php print t('Use your "My library" profile to use the online services.') ?></p>
+
+    <a class="btn btn--medium btn--alpha" href="<?php print $register_url ?>" target="_blank"><?php print t('Register') ?></a>
+    <a class="btn btn--medium btn--beta"  href="<?php print $login_url?>" target="_blank"><?php print t('Log in') ?></a>
+  </div>
+</div>
