4bf7a7746ff5f0149d11f681a2b8374677ff2940
Filip Heymans <filip.heymans@district01.be>
2015-04-15 14:14:28 +0200

    GENTBE-956: Added newsletters group (og) overview.

---
+++ templates/gent_newsletter/gent_newsletter_teaser.tpl.php
@@ -0,0 +1,8 @@
+<article id="<?php print $wrapper_id; ?>" class="newsletter-teaser l-third island island--eta js-height clearfix">
+  <div class="header">
+    <h3><?php print $title; ?></h3>
+    <?php print $subscribe_link; ?>
+  </div>
+  <?php print $messages; ?>
+  <p><?php print $description; ?></p>
+</article>
