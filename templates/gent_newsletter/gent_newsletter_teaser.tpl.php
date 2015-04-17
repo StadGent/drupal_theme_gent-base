b61bb99da7e8187b16644ef28429b9652283e879
Filip Heymans <filip.heymans@district01.be>
2015-04-17 15:51:29 +0200

    GENTBE-956: Added styling for image buttons and newsletter teaser lock.

--- templates/gent_newsletter/gent_newsletter_teaser.tpl.php
+++ templates/gent_newsletter/gent_newsletter_teaser.tpl.php
@@ -1,5 +1,5 @@
 <article id="<?php print $wrapper_id; ?>" class="newsletter-teaser l-third island island--eta js-height clearfix">
-  <div class="header">
+  <div class="header clearfix">
     <h3><?php print $title; ?></h3>
     <?php print $subscribe_link; ?>
   </div>
