cd506042c287b7e7f64e711948224479a127e296
Filip Heymans <filip.heymans@district01.be>
2015-04-17 16:26:33 +0200

    GENTBE-956: Added mobile support.

--- templates/gent_newsletter/gent_newsletter_teaser.tpl.php
+++ templates/gent_newsletter/gent_newsletter_teaser.tpl.php
@@ -1,7 +1,9 @@
 <article id="<?php print $wrapper_id; ?>" class="newsletter-teaser l-third island island--eta js-height clearfix">
   <div class="header clearfix">
-    <h3><?php print $title; ?></h3>
-    <?php print $subscribe_link; ?>
+    <h3 class="title"><?php print $title; ?></h3>
+    <div class="subscribe">
+      <?php print $subscribe_link; ?>
+    </div>
   </div>
   <?php print $messages; ?>
   <p><?php print $description; ?></p>
