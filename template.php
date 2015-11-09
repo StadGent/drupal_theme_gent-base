494c02447bddf1628d5aa1a8aacfbd8e93c09cec
Filip Heymans <filip.heymans@district01.be>
2015-11-09 15:01:11 +0100

    GENTBE-1405: Fixed webform suffix is printed before the input field instead of after.

--- template.php
+++ template.php
@@ -599,7 +599,7 @@ function gent_base_webform_element($variables) {
     case 'before':
     case 'invisible':
       $output .= ' ' . theme('form_element_label', $variables);
-      $output .= ' ' . $prefix . $suffix . '<span class="children">' . $element['#children'] . '</span>' . "\n";
+      $output .= ' ' . $prefix . '<span class="children">' . $element['#children'] . '</span>' . $suffix . "\n";
       break;
 
     case 'after':
