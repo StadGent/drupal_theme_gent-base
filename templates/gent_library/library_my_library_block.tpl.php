d834bba30830828f1fbc47a756928a5bcbbdce44
Pieter Jan <pj@codelab42.com>
2016-06-20 13:20:27 +0200

    GENTBE-1855: Added conditional more link to library block.

--- templates/gent_library/library_my_library_block.tpl.php
+++ templates/gent_library/library_my_library_block.tpl.php
@@ -5,5 +5,6 @@
 
     <a class="btn btn--medium btn--alpha" href="<?php print $register_url ?>" target="_blank"><?php print t('Register on My library') ?></a>
     <a class="btn btn--medium btn--beta"  href="<?php print $login_url?>" target="_blank"><?php print t('Log in to My library') ?></a>
+    <?php if ($more_url) : ?><a href="<?php print $more_url ?>" target="_blank"><?php print t('More info') ?></a><?php endif; ?>
   </div>
 </div>
