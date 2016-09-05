de27581096638b96ea566d59b2f7c7215555f15e
Pieter Jan <pj@codelab42.com>
2016-09-05 11:16:24 +0200

    GENTBE-1608: Update for slideshow template.

--- templates/gent_layout/gent_slideshow.tpl.php
+++ templates/gent_layout/gent_slideshow.tpl.php
@@ -1,20 +1,22 @@
-<?php if ($display): ?>
+<?php if ($items): ?>
 <div id="field-slideshow-wrapper" class="field-slideshow-wrapper clearfix">
-  <?php if ($item_count > 0): ?>
-    <?php $item = array_shift($items); ?>
-    <div class="l-full <?php if (!$item['title']): ?>padding-bottom<?php endif;?>">
-      <div class="mask ratio--big-thumb">
-        <a rel="gallery-1" href="<?php print $item['image_full'];?>" class="swipebox" title="<?php echo $item['title'];?>">
-         <img src="<?php print $item['image_medium'];?>" alt="<?php echo $item['alt'];?>" <?php print $item['attributes']; ?> />
-        </a>
+  <?php if ($item = reset($items)): ?>
+    <?php if (isset($item['image_medium'])): ?>
+      <?php $item = array_shift($items); ?>
+      <div class="l-full <?php if (!$item['title']): ?>padding-bottom<?php endif;?>">
+        <div class="mask ratio--big-thumb">
+          <a rel="gallery-1" href="<?php print $item['image_full'];?>" class="swipebox" title="<?php echo $item['title'];?>">
+           <img src="<?php print $item['image_medium'];?>" alt="<?php echo $item['alt'];?>" <?php print $item['attributes']; ?> />
+          </a>
+        </div>
+        <?php if ($item['title']): ?>
+        <p class="caption"><?php echo $item['title'];?></p>
+        <?php endif; ?>
       </div>
-      <?php if ($item['title']): ?>
-      <p class="caption"><?php echo $item['title'];?></p>
-      <?php endif; ?>
-    </div>
+    <?php endif; ?>
   <?php endif; ?>
 
-  <?php if ($item_count > 1): ?>
+  <?php if ($items): ?>
     <?php foreach ($items as $num => $item): ?>
     <div class="l-thumb">
       <div class="mask ratio--thumb">
