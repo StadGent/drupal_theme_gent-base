105ee1e0139dacf24ef3eeaba958798259d4cab9
Jeroen Goossens <jeroen@pau.be>
2016-09-13 11:43:04 +0200

    GENTBE-1433, GENTBE-1940: Fixed css search.

--- js/search-widget.js
+++ js/search-widget.js
@@ -30,6 +30,19 @@
     this.closeHandler = null;
   }
 
+  // Clearable input
+  function tog(v){
+    return v?'addClass':'removeClass';
+  }
+  $(document).on('input', '.search-widget__input', function(){
+    $(this)[tog(this.value)]('x');
+  }).on('mousemove', '.x', function( e ){
+    $(this)[tog(this.offsetWidth-18 < e.clientX-this.getBoundingClientRect().left)]('onX');
+  }).on('touchstart click', '.onX', function( ev ){
+    ev.preventDefault();
+    $(this).removeClass('x onX').val('').change();
+  });
+
   /**
    * Initialises the search widget.
    */
