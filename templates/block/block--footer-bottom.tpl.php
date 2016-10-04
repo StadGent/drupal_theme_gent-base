d9c99fe94d2fc7b7eb147aabbfab635558000145
Filip Heymans <filip.heymans@district01.be>
2016-10-04 17:21:14 +0200

    GENTBE-1051: Changed logo positioning in footer (align right + coorporation one-line).

--- templates/block/block--footer-bottom.tpl.php
+++ templates/block/block--footer-bottom.tpl.php
@@ -1,4 +1,4 @@
-<div id="<?php print $block_html_id; ?>" class="l-column island island--gamma <?php print $classes; ?>">
+<div id="<?php print $block_html_id; ?>" class="l-column js-height island island--gamma <?php print $classes; ?>">
 
 <?php if ($block->subject): ?>
   <h3><?php print $block->subject ?></h3>
--- templates/system/region--footer-bottom.tpl.php
+++ templates/system/region--footer-bottom.tpl.php
@@ -33,10 +33,12 @@
 <?php endif; ?>
 
 <?php // @TODO Make me dynamic in gent_base_tools ?>
-<div class="l-column">
+<div class="l-column js-height island island--gamma" style="position: relative;">
   <div class="brand__footer__wrapper">
     <p class="brand__footer">Stad Gent</p>
-    <p class="brand__footer__sub__text"><?php print t('In cooperation with')?></p>
-    <p class="brand__footer__sub__logo"><a href="http://www.digipolis.be">Digipolis</a></p>
+    <p class="brand__footer__sub">
+      <span class="brand__footer__sub__text"><?php print t('In cooperation with')?></span>
+      <span class="brand__footer__sub__logo"><a href="http://www.digipolis.be">Digipolis</a></span>
+    </p>
   </div>
 </div>
