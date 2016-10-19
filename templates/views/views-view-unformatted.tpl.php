cbc2fbfecfcdd6621b5fa6628a89f89fae0e9ec1
Filip Heymans <filip.heymans@district01.be>
2016-10-19 16:06:10 +0200

    GENTBE-2186: Fixed wrong floating in some views (solution: rows with no classes should never print a wrapper div, row itself contains layout classes).

--- templates/views/views-view-unformatted.tpl.php
+++ templates/views/views-view-unformatted.tpl.php
@@ -8,17 +8,21 @@
  */
 ?>
 <?php if (!empty($title)): ?>
-<header class="l-full">
-	<h2 class="h1 hT"><?php print $title; ?></h2>
-</header>
+  <header class="l-full">
+    <h2 class="h1 hT"><?php print $title; ?></h2>
+  </header>
 <?php endif; ?>
 
 <?php if ($rows): ?>
   <div class="view-content clearfix">
-  <?php foreach ($rows as $id => $row): ?>
-    <div<?php if (isset($classes_array[$id]) && $classes_array[$id]) { print ' class="' . $classes_array[$id] .'"';  } ?>>
-      <?php print $row; ?>
-    </div>
-  <?php endforeach; ?>
+    <?php foreach ($rows as $id => $row): ?>
+      <?php if (!empty($classes_array[$id])): ?>
+        <div class="<?php print $classes_array[$id]; ?>">
+          <?php print $row; ?>
+        </div>
+      <?php else: ?>
+        <?php print $row; ?>
+      <?php endif; ?>
+    <?php endforeach; ?>
   </div>
 <?php endif; ?>
