d4a38442b71be6475e7987a28a181666f4ebf83c
Floris Walraet <floris.walraet@digipolis.gent>
2016-12-02 14:35:28 +0100

    DTGB-12: Refactored accordion JS

--- js/theme.js
+++ js/theme.js
@@ -39,7 +39,6 @@
       jsTheme.forms.init();
       jsTheme.searchThemes.init();
       jsTheme.progressAnimator.init();
-      jsTheme.accordion.init();
       jsTheme.toggleFieldset.init();
     }
   };
@@ -92,40 +91,6 @@
         $(this).animate({
           width: targetWidth + '%'
         }, duration, 'linear');
-      });
-    }
-  };
-
-  /**
-   *
-   * @type {{init: jsTheme.accordion.init}}
-   */
-  jsTheme.accordion = {
-    init: function () {
-      // Code for simple accordion animations
-      $('.accordion').each(function () {
-        // Close all contents except the first
-        $('.accordion-item-content', this).hide();
-        var activeItem = $('.accordion-item:first', this);
-        activeItem.find('.accordion-item-content').show();
-        activeItem.addClass('active').find('.accordion-item-title span').toggleClass('icon-arrow-down icon-arrow-up');
-
-        // Now toggle items on click
-        $('.accordion-item-title a', this).click(function (e) {
-          e.preventDefault();
-          var currentItem = $(this).parents('.accordion-item');
-
-          if (currentItem.hasClass('active')) {
-            currentItem.removeClass('active');
-            currentItem.find('.accordion-item-content').slideUp('slow');
-            currentItem.find('.accordion-item-title span').toggleClass('icon-arrow-up icon-arrow-down');
-          }
-          else {
-            currentItem.addClass('active');
-            currentItem.find('.accordion-item-content').slideDown('slow');
-            currentItem.find('.accordion-item-title span').toggleClass('icon-arrow-down icon-arrow-up');
-          }
-        });
       });
     }
   };
