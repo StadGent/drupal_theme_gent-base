52ff4cf6b89eb7ad3b849ff8d89272b9e4b832c3
Floris Walraet <floris.walraet@digipolis.gent>
2016-12-16 14:12:47 +0100

    DTGB-1: Moved stad.gent code, Fixed grunt tasks

--- templates/gent_wijksites/gent-wijksites-wvdm-home.tpl.php
+++
@@ -1,6 +0,0 @@
-<div class="l-half island island--alpha js-height">
-	<h3 class="brand__district" <?php if ($image_url): ?>>style="background-image:"url('<?php print $image_url;?>')"<?php endif; ?>><?php print t('Wijk van de maand')?></h3>
-	<h4><?php print $title;?></h4>
-	<p><?php print $description;?></p>
-	<p><?php print $link;?></p>
-</div>
