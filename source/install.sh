6298c32f214eb6b8d03e77f36dc9c6ca9c382622
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2017-08-30 10:22:29 +0200

    Fixed install script.

--- source/install.sh
+++ source/install.sh
@@ -8,8 +8,8 @@ rm -rf ../build
 
 echo "Building the Gent basetheme..."
 mkdir ../build && mkdir ../build/styleguide;
-mv ./node_modules/gent_styleguide/build/styleguide/fonts ../build/styleguide/fonts
-mv ./node_modules/gent_styleguide/build/styleguide/img ../build/styleguide/img
-mv ./node_modules/gent_styleguide/build/styleguide/js ../build/styleguide/js
+cp -R ./node_modules/gent_styleguide/build/styleguide/fonts ../build/styleguide/fonts
+cp -R ./node_modules/gent_styleguide/build/styleguide/img ../build/styleguide/img
+cp -R ./node_modules/gent_styleguide/build/styleguide/js ../build/styleguide/js
 
 echo "The Gent basetheme is successfully installed!"
