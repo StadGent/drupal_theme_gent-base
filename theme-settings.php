045452796e21cf840356fcdfefcc798c08773d2a
Wim Vantomme <wim.vantomme@daraja.be>
2017-10-04 11:41:05 +0200

    DTGB-52 added typekit integration with theme-setting

--- theme-settings.php
+++ theme-settings.php
@@ -7,19 +7,26 @@
 
  use Drupal\Core\Form\FormStateInterface;
 
- /**
-  * Implements hook_form_system_theme_settings_alter().
-  */
-  function gent_base_form_system_theme_settings_alter(&$form, FormStateInterface $form_state, $form_id = NULL) {
-    // Work-around for a core bug affecting admin themes. See issue #943212.
-    if (isset($form_id)) {
-      return;
-    }
-
-    $form['gent_base'] = [
-      '#type' => 'details',
-      '#title' => t('Gent Base settings'),
-      '#description' => t('All settings defined by the Gent Base styleguide.'),
-      '#open' => TRUE,
-    ];
+/**
+ * Implements hook_form_system_theme_settings_alter().
+ */
+function gent_base_form_system_theme_settings_alter(&$form, FormStateInterface $form_state, $form_id = NULL) {
+  // Work-around for a core bug affecting admin themes. See issue #943212.
+  if (isset($form_id)) {
+    return;
   }
+
+  $form['gent_base'] = [
+    '#type' => 'details',
+    '#title' => t('Gent Base settings'),
+    '#description' => t('All settings defined by the Gent Base styleguide.'),
+    '#open' => TRUE,
+  ];
+
+  $form['gent_base']['typekit_id'] = [
+    '#type' => 'textfield',
+    '#title' => t('Adobe Typekit-ID'),
+    '#default_value' => theme_get_setting('typekit_id'),
+    '#description' => t('Fill in the Adobe Typekit-ID to be used in your project'),
+  ];
+}
