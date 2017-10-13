64b430a2ef51d881ad0f55b078425ef329320454
Pieter Jan <pj.claeys@gmail.com>
2017-10-13 09:53:20 +0200

    DTGB-111: Removed expressway font and replaced by typekit integration.

--- templates/system/html.tpl.php
+++ templates/system/html.tpl.php
@@ -19,6 +19,21 @@
 
   <link href="//fonts.googleapis.com/css?family=Source+Sans+Pro:400,700,400italic" rel="stylesheet" type="text/css" />
 
+  <?php if (!empty($type_kit_id)) :?>
+  <!-- Load Typekit -->
+  <script>
+    (function(d) {
+      var config = {
+          kitId: '<?php print $type_kit_id; ?>',
+          scriptTimeout: 3000,
+          async: true
+        },
+        h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)
+    })(document);
+  </script>
+  <?php endif; ?>
+
+
   <?php print $styles; ?>
 
   <?php if (isset($head_scripts)): ?>
