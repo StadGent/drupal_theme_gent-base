c1449a429daf4c9dba515764ec01b47ac59e46de
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2017-10-19 14:47:21 +0200

    DTGB-116: Added vendor packages.

--- source/install.sh
+++ source/install.sh
@@ -7,11 +7,11 @@ echo "Removing old build directory..."
 rm -rf ../build
 
 echo "Building the Gent basetheme..."
-mkdir ../build && mkdir ../build/styleguide && mkdir ../build/js && mkdir ../build/vendor;
-cp -R ./node_modules/gent_styleguide/build/styleguide/fonts ../build/styleguide/fonts
-cp -R ./node_modules/gent_styleguide/build/styleguide/img ../build/styleguide/img
-cp -R ./node_modules/gent_styleguide/build/styleguide/js ../build/styleguide/js
-cp -R ./node_modules/lightgallery/ ../build/vendor/lightgallery
+mkdir ../build && mkdir ../build/styleguide && mkdir ../build/js && mkdir;
+cp -R ./node_modules/gent_styleguide/build/styleguide/fonts ../build/styleguide/fonts;
+cp -R ./node_modules/gent_styleguide/build/styleguide/img ../build/styleguide/img;
+cp -R ./node_modules/gent_styleguide/build/styleguide/js ../build/styleguide/js;
+cp -R ./node_modules/gent_styleguide/build/styleguide/vendor ../build/styleguide/vendor;
 
 echo "Building Gent basetheme js..."
 gulp build
