07268fa844fdfcc29c4fa1af6e07b4d7b25a7f82
Filip Heymans <filip.heymans@district01.be>
2018-02-13 15:28:54 +0100

    Fixed jslint warnings for Gruntfile.js.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,152 +1,157 @@
-'use strict';
+(function () {
 
-module.exports = function (grunt) {
+  'use strict';
 
-  grunt.initConfig({
-    watch: {
-      options: {
-        livereload: true
-      },
-      sass: {
-        files: ['sass/{,**/}*.{scss,sass}'],
-        tasks: ['sasslint', 'compass:dev'],
+  /* global module */
+  module.exports = function (grunt) {
+
+    grunt.initConfig({
+      watch: {
         options: {
-          livereload: false
-        }
-      },
-      registry: {
-        files: ['*.info', '{,**}/*.{php,inc}'],
-        tasks: ['shell'],
-        options: {
-          livereload: false
-        }
-      },
-      images: {
-        files: ['img/**']
-      },
-      css: {
-        files: ['css/{,**/}*.css']
-      },
-      js: {
-        files: ['js/{,**/}*.js', '!js/{,**/}*.min.js'],
-        tasks: ['jshint', 'uglify:dev']
-      }
-    },
-
-    shell: {
-      all: {
-        command: 'drush cache-clear theme-registry'
-      }
-    },
-
-    compass: {
-      options: {
-        config: 'config.rb',
-        bundleExec: true,
-        force: true
-      },
-      dev: {
-        options: {
-          environment: 'development'
-        }
-      },
-      dist: {
-        options: {
-          environment: 'production'
-        }
-      }
-    },
-
-    jshint: {
-      options: {
-        jshintrc: '.jshintrc'
-      },
-      all: ['js/{,**/}*.js', '!js/{,**/}*.min.js']
-    },
-
-    sasslint: {
-      options: {
-        configFile: '.sass-lint.yml'
-      },
-      target: ['sass/**/*.s+(a|c)ss']
-    },
-
-    uglify: {
-      dev: {
-        options: {
-          mangle: false,
-          compress: false,
-          beautify: true
+          livereload: true
         },
-        files: [{
-          expand: true,
-          flatten: true,
-          cwd: 'js',
-          dest: 'js',
-          src: ['**/*.js', '!**/*.min.js'],
-          rename: function(dest, src) {
-            var folder = src.substring(0, src.lastIndexOf('/'));
-            var filename = src.substring(src.lastIndexOf('/'), src.length);
-            filename = filename.substring(0, filename.lastIndexOf('.'));
-            return dest + '/' + folder + filename + '.min.js';
+        sass: {
+          files: ['sass/{,**/}*.{scss,sass}'],
+          tasks: ['sasslint', 'compass:dev'],
+          options: {
+            livereload: false
           }
-        }]
-      },
-      dist: {
-        options: {
-          mangle: true,
-          compress: {}
         },
-        files: [{
-          expand: true,
-          flatten: true,
-          cwd: 'js',
-          dest: 'js',
-          src: ['**/*.js', '!**/*.min.js'],
-          rename: function(dest, src) {
-            var folder = src.substring(0, src.lastIndexOf('/'));
-            var filename = src.substring(src.lastIndexOf('/'), src.length);
-            filename = filename.substring(0, filename.lastIndexOf('.'));
-            return dest + '/' + folder + filename + '.min.js';
+        registry: {
+          files: ['*.info', '{,**}/*.{php,inc}'],
+          tasks: ['shell'],
+          options: {
+            livereload: false
           }
-        }]
-      },
-      // Extra uglifying for libs that don't ship with a minified version.
-      // Run after bower update.
-      libs: {
-        options: {
-          mangle: true,
-          compress: true
         },
-        files: [
-          {'libraries/jquery-sticky/jquery.sticky.min.js': 'libraries/jquery-sticky/jquery.sticky.js'}
-        ]
+        images: {
+          files: ['img/**']
+        },
+        css: {
+          files: ['css/{,**/}*.css']
+        },
+        js: {
+          files: ['js/{,**/}*.js', '!js/{,**/}*.min.js'],
+          tasks: ['jshint', 'uglify:dev']
+        }
+      },
+
+      shell: {
+        all: {
+          command: 'drush cache-clear theme-registry'
+        }
+      },
+
+      compass: {
+        options: {
+          config: 'config.rb',
+          bundleExec: true,
+          force: true
+        },
+        dev: {
+          options: {
+            environment: 'development'
+          }
+        },
+        dist: {
+          options: {
+            environment: 'production'
+          }
+        }
+      },
+
+      jshint: {
+        options: {
+          jshintrc: '.jshintrc'
+        },
+        all: ['js/{,**/}*.js', '!js/{,**/}*.min.js']
+      },
+
+      sasslint: {
+        options: {
+          configFile: '.sass-lint.yml'
+        },
+        target: ['sass/**/*.s+(a|c)ss']
+      },
+
+      uglify: {
+        dev: {
+          options: {
+            mangle: false,
+            compress: false,
+            beautify: true
+          },
+          files: [{
+            expand: true,
+            flatten: true,
+            cwd: 'js',
+            dest: 'js',
+            src: ['**/*.js', '!**/*.min.js'],
+            rename: function (dest, src) {
+              var folder = src.substring(0, src.lastIndexOf('/'));
+              var filename = src.substring(src.lastIndexOf('/'), src.length);
+              filename = filename.substring(0, filename.lastIndexOf('.'));
+              return dest + '/' + folder + filename + '.min.js';
+            }
+          }]
+        },
+        dist: {
+          options: {
+            mangle: true,
+            compress: {}
+          },
+          files: [{
+            expand: true,
+            flatten: true,
+            cwd: 'js',
+            dest: 'js',
+            src: ['**/*.js', '!**/*.min.js'],
+            rename: function (dest, src) {
+              var folder = src.substring(0, src.lastIndexOf('/'));
+              var filename = src.substring(src.lastIndexOf('/'), src.length);
+              filename = filename.substring(0, filename.lastIndexOf('.'));
+              return dest + '/' + folder + filename + '.min.js';
+            }
+          }]
+        },
+        // Extra uglifying for libs that don't ship with a minified version.
+        // Run after bower update.
+        libs: {
+          options: {
+            mangle: true,
+            compress: true
+          },
+          files: [
+            {'libraries/jquery-sticky/jquery.sticky.min.js': 'libraries/jquery-sticky/jquery.sticky.js'}
+          ]
+        }
       }
-    }
-  });
+    });
 
 
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-sass-lint');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-shell');
-  grunt.loadNpmTasks('grunt-newer');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    grunt.loadNpmTasks('grunt-sass-lint');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-newer');
 
-  grunt.registerTask('build', [
-    'newer:uglify:dist',
-    'sasslint',
-    'compass:dist',
-    'jshint'
-  ]);
+    grunt.registerTask('build', [
+      'newer:uglify:dist',
+      'sasslint',
+      'compass:dist',
+      'jshint'
+    ]);
 
-  grunt.registerTask('compile', [
-    'sasslint',
-    'compass:dev'
-  ]);
+    grunt.registerTask('compile', [
+      'sasslint',
+      'compass:dev'
+    ]);
 
-  grunt.registerTask('uglifybowerlibs', [
-    'uglify:libs'
-  ]);
-};
+    grunt.registerTask('uglifybowerlibs', [
+      'uglify:libs'
+    ]);
+  };
+
+})();
