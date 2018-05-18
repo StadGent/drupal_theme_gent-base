ece42c06f4201bab34abe4e9f230ca2f70036c2e
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2018-05-18 14:30:25 +0200

    DTGB-282: Added image gallery and did some refactoring of template files.

--- source/js/gallery.binding.js
+++ source/js/gallery.binding.js
@@ -1,14 +1,22 @@
 /**
  * @file
- * Gallery component binding.
+ * Javascript binding of baguettebox.min.js.
  */
 (function ($, Drupal) {
   'use strict';
 
   Drupal.behaviors.gentBaseLoadLightGallery = {
     attach: function (context, settings) {
-      $('.gallery', context).once('.loadLightGallery').each(function () {
-        $('.gallery').loadLightGallery();
+      if (!baguetteBox) { // eslint-disable-line no-undef
+        return;
+      }
+      baguetteBox.run('.image-gallery', { // eslint-disable-line no-undef
+        filter: /.*/i,
+        async: true,
+        captions: function (element) {
+          var figcaption = element.querySelector('figcaption');
+          return figcaption ? figcaption.innerText : null;
+        }
       });
     }
   };
