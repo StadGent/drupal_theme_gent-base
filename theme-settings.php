3949d2e46cfa72a79f0b3a30600c2d2026365798
Jeroen Goossens <jeroen@pau.be>
2018-08-29 13:06:47 +0200

    DTGB-518: Updated translations.

--- theme-settings.php
+++ theme-settings.php
@@ -18,31 +18,31 @@ function gent_base_form_system_theme_settings_alter(&$form, FormStateInterface $
 
   $form['gent_base'] = [
     '#type' => 'details',
-    '#title' => t('Gent Base settings'),
-    '#description' => t('All settings defined by the Gent Base styleguide.'),
+    '#title' => t("Gent Base settings"),
+    '#description' => t("All settings defined by the Gent Base styleguide."),
     '#open' => TRUE,
   ];
 
   $form['gent_base']['color_scheme'] = [
     '#type' => 'textfield',
-    '#title' => t('Style guide color scheme class'),
+    '#title' => t("Style guide color scheme class"),
     '#default_value' => theme_get_setting('color_scheme'),
     '#field_prefix' => 'cs--',
-    '#description' => t('Fill in the color scheme class to be used in your project, leave empty for the default color scheme.'),
+    '#description' => t("Fill in the color scheme class to be used in your project, leave empty for the default color scheme."),
   ];
 
   $form['gent_base']['color_scheme_hex'] = [
     '#type' => 'textfield',
-    '#title' => t('Style guide color scheme primary HEX color'),
+    '#title' => t("Style guide color scheme primary HEX color"),
     '#default_value' => theme_get_setting('color_scheme_hex'),
     '#field_prefix' => '#',
-    '#description' => t('Fill in the color scheme primary HEX color to be used in your project, leave empty for the default color scheme. This setting is only used for displaying the favicon correctly on Android + Windows phones and Apple\'s Touch Bar enabled computers.'),
+    '#description' => t("Fill in the color scheme primary HEX color to be used in your project, leave empty for the default color scheme. This setting is only used for displaying the favicon correctly on Android + Windows phones and Apple's Touch Bar enabled computers."),
   ];
 
   $form['gent_base']['typekit_id'] = [
     '#type' => 'textfield',
-    '#title' => t('Adobe Typekit-ID'),
+    '#title' => t("Adobe Typekit-ID"),
     '#default_value' => theme_get_setting('typekit_id'),
-    '#description' => t('Fill in the Adobe Typekit-ID to be used in your project.'),
+    '#description' => t("Fill in the Adobe Typekit-ID to be used in your project."),
   ];
 }
