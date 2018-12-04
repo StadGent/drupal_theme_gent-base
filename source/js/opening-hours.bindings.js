9652925279ada9520050b039a68a9ffc14b7fe80
Helena Standaert <helena.standaert@gmail.com>
2018-12-04 13:47:34 +0100

    DTGB-632: Open accordion when only 1 channel is available

--- source/js/opening-hours.bindings.js
+++ source/js/opening-hours.bindings.js
@@ -7,6 +7,30 @@
 
   Drupal.behaviors.gentBaseLoadOpeningHours = {
     attach: function (context, settings) {
+      this.initMonthWidgets();
+      this.determineAccordionVisibility();
+    },
+
+    /**
+     * Determine whether or not the accordion should be expanded on page load
+     */
+    determineAccordionVisibility: function () {
+      var selected = document.querySelectorAll('.opening-hours-accordion');
+      [].slice.call(selected).forEach(function (accordion) {
+        var items = accordion.querySelectorAll('.opening-hours-accordion__item');
+        if (items.length === 1) {
+          var widget = accordion.querySelector('.openinghours-widget:last-of-type');
+          var eventHandler = function (ev) {
+            items[0].querySelector('.accordion--button').click();
+            widget.removeEventListener('change', eventHandler);
+          };
+
+          widget.addEventListener('change', eventHandler);
+        }
+      });
+    },
+
+    initMonthWidgets: function () {
       if (!OpeningHoursMonthWidget) { // eslint-disable-line no-undef
         return;
       }
