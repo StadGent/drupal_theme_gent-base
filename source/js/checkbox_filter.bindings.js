2f4f690a0b1258549e5b6e992a67217844472611
Helena Standaert <helena.standaert@gmail.com>
2018-12-24 11:02:38 +0100

    Revert 76d4276e02150d0f8bc69fb91596a79036ca23e7

--- source/js/checkbox_filter.bindings.js
+++ source/js/checkbox_filter.bindings.js
@@ -17,6 +17,7 @@
           checkboxes: '.facet-item.checkbox',
           hiddenTagText: 'Remove tag',
           countSpan: '.checkbox-filter__count-wrapper em',
+          resultSpan: '.checkbox-filter__result-wrapper em',
           onRemoveTag: function (checkbox) {
             jQuery(checkbox).trigger('change.facets');
           }
