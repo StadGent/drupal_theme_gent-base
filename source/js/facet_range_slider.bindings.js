65987fe7cccf2795f6c066e391f7fe9d6271acd5
Helena Standaert <helena.standaert@gmail.com>
2018-12-27 15:15:13 +0100

    DTGB-657: Moved all facet_range_slider related code into its own bindings file to avoid the code from being loaded on all filter pages

--- source/js/facet_range_slider.bindings.js
+++ source/js/facet_range_slider.bindings.js
@@ -5,8 +5,52 @@
 (function ($, Drupal) {
   'use strict';
 
-  Drupal.behaviors.gentBaseAlterFacetRangeSliders = {
+  /**
+   * Override facet slider behavior
+   */
+  Drupal.behaviors.facet_slider = {
     attach: function (context, settings) {
+      if (typeof settings.facets !== 'undefined' && typeof settings.facets.sliders !== 'undefined') {
+        var self = this;
+
+        $.each(settings.facets.sliders, function (facet, settings) {
+          self.addUrlFragment(facet, settings);
+          Drupal.facets.addSlider(facet, settings);
+        });
+
+        this.updateCopy();
+      }
+    },
+
+    /**
+     * Add suffix to links inside modals to re-open them when the page reloads
+     *
+     * @param {string} facet
+     *  Id of the facet that should be altered.
+     * @param {object} settings
+     *  Settings object that contains the URL that should be altered.
+     */
+    addUrlFragment: function (facet, settings) {
+      settings = settings || {};
+      if (!settings.url) {
+        return;
+      }
+
+      var $facet = document.querySelector('[id^="' + facet + '"][id$="' + facet + '"]');
+      var $modal = $facet.closest('.modal');
+
+      if (!$modal) {
+        return;
+      }
+
+      settings.url = settings.url.split('#')[0]; // Strip hash if there was one already
+      settings.url += '#' + $modal.id;
+    },
+
+    /**
+     * Update copy dynamically when the user changes the value of the slider.
+     */
+    updateCopy: function () {
       var minPips = document.querySelector('.pips-preview .placeholder:first-of-type');
       var maxPips = document.querySelector('.pips-preview .placeholder:last-of-type');
       var slider = document.querySelector('.facet-slider');
@@ -20,5 +64,6 @@
       });
     }
   };
+
 })(jQuery, Drupal);
 
