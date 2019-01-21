f949d89fd402b48c6ce90241a511552398332efc
Gert-Jan Meire <gertjan.meire@digipolis.gent>
2019-01-21 13:06:19 +0100

    DTGB-675: Made label atom hidden.

---
+++ styleguide/components/21-atoms/label/label.config.js
@@ -0,0 +1,14 @@
+'use strict';
+
+module.exports = {
+  title: 'Label',
+  hidden: true,
+  status: 'ready',
+  handle: 'label',
+  order: 11,
+  context: {
+    label: 'Label',
+    for: 'form-item',
+    label_optional: 'Optional'
+  }
+};
