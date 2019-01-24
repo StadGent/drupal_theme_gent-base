d0fbb5de073354181e1aac8cd92747f199f9f89a
Helena Standaert <helena.standaert@gmail.com>
2019-01-24 11:24:40 +0100

    DTGB-491: Fixed linting errors

--- source/js/dg_maps.bindings.js
+++ source/js/dg_maps.bindings.js
@@ -14,8 +14,8 @@
       }
 
       var selected = document.querySelectorAll('.js-map-tabs');
-      for (var i = selected.length; i--;) {
-        allieTabs.init(selected[i], {
+      for (var s = selected.length; s--;) {
+        allieTabs.init(selected[s], {
           changeTab: function (tab, tabs, tabpanels, component, init) {
             for (var i = tabs.length; i > 0; i--) {
               tabs[i - 1].setAttribute('aria-selected', false);
@@ -25,8 +25,8 @@
             tab.setAttribute('aria-selected', true);
             tab.tabIndex = 0;
 
-            for (var i = tabpanels.length; i > 0; i--) {
-              tabpanels[i - 1].setAttribute('aria-hidden', true);
+            for (var x = tabpanels.length; x > 0; x--) {
+              tabpanels[x - 1].setAttribute('aria-hidden', true);
             }
 
             var tabpanel = component.querySelector(tab.hash);
