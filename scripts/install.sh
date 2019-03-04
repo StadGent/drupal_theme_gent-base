9002a114b2299f75898e5f52062004130b39c87f
Helena Standaert <helena.standaert@gmail.com>
2019-03-04 15:55:25 +0100

    DTGB-700: Don't build styleguide assets, since it should be built in the package

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,34 +12,7 @@ rm -rf ../build;
 echo "Recreating gent_base 'build' directory...";
 mkdir ../build && mkdir ../build/js;
 
-echo "Building - style guide - in gent_base...";
-cd ../styleguide;
-yarn install;
-
-echo "Generating iconfont...";
-./node_modules/.bin/gulp iconfont;
-
-echo "Generating spot images...";
-./node_modules/.bin/gulp spotimages;
-
-echo "Copy public files to build...";
-mkdir build;
-cp -rf ./public/** ./build;
-
-echo "Extracting styles...";
-./node_modules/.bin/gulp styles:extract;
-
-echo "Building JS...";
-./node_modules/.bin/gulp js:build;
-
-echo "Minifying images...";
-./node_modules/.bin/gulp images:minify;
-
-echo "Executing - style guide - postinstall script...";
-cd scripts;
-node ./postinstall.js;
-
 echo "Building gent_base...";
-cd ../../source;
+cd ../source;
 yarn install;
 ./node_modules/.bin/gulp build;
