9b2a77252d24966e93d4823eab4d5f58973de339
Helena Standaert <helena.standaert@gmail.com>
2019-03-04 10:44:57 +0100

    DTGB-652: Scroll to bottom of the map when opening a popup

--- source/js/dg_maps.popup.js
+++ source/js/dg_maps.popup.js
@@ -44,5 +44,22 @@
       this.info_.push(Mustache.render(template, features[i].getProperties()));
     }
   };
+
+
+  /**
+   * Shows the popup element.
+   * @param {ol.Coordinate} coordinate Coordinates.
+   * @private
+   */
+  var originalFunction = Drupal.dgMaps.ol.interaction.Popup.prototype.openPopup_;
+  Drupal.dgMaps.ol.interaction.Popup.prototype.openPopup_ = function (coordinate) {
+    originalFunction.apply(this, arguments); // Execute the module's function.
+
+    // When the bottom of the popup is not in the viewport, scroll down.
+    this.popupContainer_.scrollIntoView({
+      block: 'nearest',
+      behavior: 'smooth'
+    })
+  };
 })(jQuery, Drupal);
 
