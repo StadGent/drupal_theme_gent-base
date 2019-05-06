85ca6d1b62c5507e47271ded4207ca81caf59882
Helena Standaert <helena.standaert@gmail.com>
2019-05-06 13:29:29 +0200

    Style file input

---
+++ source/js/file.bindings.js
@@ -0,0 +1,23 @@
+/* global File */
+
+/**
+ * @file
+ * File component binding.
+ */
+(function (Drupal) {
+  'use strict';
+
+  Drupal.behaviors.gentBaseLoadFile = {
+    attach: function (context, settings) {
+      if (!File) {
+        return;
+      }
+
+      var selected = document.querySelectorAll('.file');
+      for (var i = selected.length; i--;) {
+        new File(selected[i]);
+      }
+
+    }
+  };
+})(Drupal);
