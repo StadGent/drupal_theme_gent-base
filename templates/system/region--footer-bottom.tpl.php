a13b089317cf0d3b50141980d7d9ece96eacd362
Matthijs Van Assche <matthijs@fanatict.be>
2021-02-01 17:23:48 +0100

    Change Digipolis logo to District09

--- templates/system/region--footer-bottom.tpl.php
+++ templates/system/region--footer-bottom.tpl.php
@@ -12,6 +12,6 @@
   <div class="brand__footer__wrapper l-full">
     <p class="brand__footer__sub">
       <span class="brand__footer__sub__text"><?php print t('In cooperation with')?></span>
-      <span class="brand__footer__sub__logo"><a href="http://www.digipolis.be">Digipolis</a></span>
+      <span class="brand__footer__sub__logo"><a href="https://district09.gent">District09</a></span>
     </p>
   </div>
