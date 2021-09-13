11cfbed05aacead7aac4a5f7502998fde0009c9d
Jeroen Degloire <jeroen.degloire@hotmail.be>
2021-09-13 10:14:38 +0200

    DTGB-863: Fix normalize conflict on header accolade
    DTGB-852: Fix dynamic checkbox component
    
    DTGB-862: Fix postinstall issue on jenkins

--- source/js/checkbox_filter.bindings.js
+++ source/js/checkbox_filter.bindings.js
@@ -7,20 +7,30 @@
 
   Drupal.behaviors.gentBaseLoadCheckboxFilters = {
     attach: function (context, settings) {
-      if (!CheckboxFilterDynamic) { // eslint-disable-line no-undef
-        return;
-      }
-
-      $('.checkbox-filter', context).once('checkbox-filter').each(function () {
-        new CheckboxFilterDynamic(this, { // eslint-disable-line no-undef
-          checkboxes: '.facet-item.checkbox',
-          hiddenTagText: 'Remove tag',
-          countSpan: '.checkbox-filter__count-wrapper em',
-          resultSpan: '.checkbox-filter__result-wrapper em',
-          onRemoveTag: function (checkbox) {
-            $(checkbox).trigger('change.facets');
+      $('.checkbox-accordion', context).once('checkbox-accordion').each(function () {
+        if (Accordion) { // eslint-disable-line no-undef
+          const selected = document.querySelectorAll('.checkbox-accordion');
+          for (let i = selected.length; i--;) {
+            new Accordion(selected[i]); // eslint-disable-line no-undef
           }
-        });
+        }
+      });
+
+      $('.checkbox-filter-dynamic', context).once('checkbox-filter-dynamic').each(function () {
+        if (CheckboxFilterDynamic) { // eslint-disable-line no-undef
+          const selected = document.querySelectorAll('.checkbox-filter-dynamic');
+          for (let i = selected.length; i--;) {
+            new CheckboxFilterDynamic(selected[i], { // eslint-disable-line no-undef
+              checkboxes: '.facet-item.checkbox',
+              hiddenTagText: 'Remove tag',
+              resultSpan: '.checkbox-filter__result-wrapper em',
+              countSpan: '.checkbox-filter__count-wrapper em',
+              onRemoveTag: function (checkbox) {
+                $(checkbox).trigger('change.facets');
+              }
+            });
+          }
+        }
       });
     }
   };
