a921cd800f4a78f409e34735815359ff57dc5e5d
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2023-08-10 10:36:23 +0200

    Update install script for composer
    Add support for nvm.

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+NPM_COMMAND=(npm install)
+
 # Check if npm is installed on your system.
 if ! [ -x "$(command -v npm)" ]; then
   echo 'Error: npm is not installed. Please install npm globally to execute this script.' >&2
   exit 1
+fi
+
+# Check if nvm is available
+if ! [ -x "$(command -v nvm)" ]; then
+  if [ -f ~/.nvm/nvm.sh ]; then
+    . ~/.nvm/nvm.sh;
+    NPM_COMMAND=(nvm exec npm install);
+  fi
+else
+  NPM_COMMAND=(nvm exec npm install);
 fi
 
 echo "Removing old gent_base 'build' directory...";
@@ -14,7 +26,7 @@ mkdir ../build;
 
 echo "Building gent_base...";
 cd ../source;
-npm install;
+"${NPM_COMMAND[@]}"
 mkdir -p ../build/@digipolis-gent/modal;
 cp -R ./node_modules/@digipolis-gent/modal/dist/index.js ../build/@digipolis-gent/modal;
 cp -R ./node_modules/gent_styleguide/build/styleguide ../build;
