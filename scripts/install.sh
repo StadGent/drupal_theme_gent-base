6ba42ce0f03a696ca139c017ad15de9781a158e7
Jelle Sebreghts <sebreghts.jelle@gmail.com>
2023-08-10 15:17:34 +0200

    Fix install script for travis

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,14 +8,21 @@ if ! [ -x "$(command -v npm)" ]; then
   exit 1
 fi
 
-# Check if nvm is available
-if ! [ -x "$(command -v nvm)" ]; then
-  if [ -f ~/.nvm/nvm.sh ]; then
-    . ~/.nvm/nvm.sh;
+# Check if we're on Travis, `nvm use` should be part of the Travis pre-install
+# task.
+if [ -z "$TRAVIS" ]; then
+  # Check if nvm is available
+  if ! [ -x "$(command -v nvm)" ]; then
+    echo "nvm command not found, trying to load ~/.nvm/nvm.sh.";
+    if [ -f ~/.nvm/nvm.sh ]; then
+      echo "~/.nvm/nvm.sh found, loading it.";
+      . ~/.nvm/nvm.sh;
+      NPM_COMMAND=(nvm exec npm install);
+    fi
+  else
+    echo "nvm command found."
     NPM_COMMAND=(nvm exec npm install);
   fi
-else
-  NPM_COMMAND=(nvm exec npm install);
 fi
 
 echo "Removing old gent_base 'build' directory...";
